2c65b88a574ce12b48a8f572ec1566db1fcbb407
Lxb <905637031@qq.com>
2020-09-11 23:39:27 +0800

    推荐页面

--- src/components/top-ranking/index.js
+++ src/components/top-ranking/index.js
@@ -1,8 +1,8 @@
 import React, { memo } from 'react'
-// import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 import { getSizeImage } from '@/utils/format-utils'
-// import { getSongDetailAction } from '@/pages/player/store'
+import { getSongDetailAction } from '@/pages/player/store'
 
 import { TopRankingWrapper } from './style'
 
@@ -10,11 +10,11 @@ export default memo(function LxbTopRanking(props) {
     const { info } = props
     const { tracks = [] } = info
 
-    // const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-    // const playMusic = (item) => {
-    //     dispatch(getSongDetailAction(item.id))
-    // }
+    const playMusic = (item) => {
+        dispatch(getSongDetailAction(item.id))
+    }
 
     return (
         <TopRankingWrapper>
@@ -35,18 +35,23 @@ export default memo(function LxbTopRanking(props) {
                     {
                         tracks.slice(0, 10).map((item, index) => {
                             return (
-                                <div key={ item.id } className="list-item">
-                                    <div className="rank">{ index + 1 }</div>
-                                    <div className="info">
-                                        <span className="name text-nowrap">{ item.name }</span>
-                                        <div className="operate">
-                                            <button className="btn sprite_02 play"></button>
-                                            <button className="btn sprite_icon2 addto"></button>
-                                            <button className="btn sprite_02 favor"></button>
-                                        </div>
-                                    </div>
+                              <div key={item.id} className="list-item">
+                                <div className="rank">{index + 1}</div>
+                                <div className="info">
+                                  <span className="name text-nowrap">
+                                    {item.name}
+                                  </span>
+                                  <div className="operate">
+                                    <button
+                                      className="btn sprite_02 play"
+                                      onClick={(e) => playMusic(item)}
+                                    ></button>
+                                    <button className="btn sprite_icon2 addto"></button>
+                                    <button className="btn sprite_02 favor"></button>
+                                  </div>
                                 </div>
-                            )
+                              </div>
+                            );
                         })
                     }
                 </div>
